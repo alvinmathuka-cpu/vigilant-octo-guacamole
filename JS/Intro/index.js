f408752179198cc0a2aea0d7034fa4853d00a343
alvinmathuka-cpu <alvin.mathuka@student.moringaschool.com>
2026-06-18 13:21:12 +0300

    data types commit

--- JS/Intro/index.js
+++ JS/Intro/index.js
@@ -7,3 +7,10 @@ myage=21
 myagein20yrs=41
 Myage=21
 //!never start with numbers or reserved keywords
+myageinthenext5yrs=26 //hard to read
+//Pascal Case
+MyAgeInTheNext5Yrs=26
+//camel Case
+myAgeInTheNext5Yrs=26
+//snake_case
+my_age_in_the_next_5_yrs=26
---
+++ JS/Intro/primitiveDataType.js
@@ -0,0 +1,67 @@
+/*
+Numbers,
+floating point <decimals>
+real
+positive
+negative
+*/
+
+let number1=23;
+console.log(number1)
+let number2=34.34;
+console.log(number2)
+let number3=-34
+console.log(number3)
+let biggestNumber=545464545479454651654678465165798978479878416978541645451941994987489798415454654678684645465496787984545464684156415645646545456216272945729615985732624621642194579267325627461264216752491279421961297421972419246752465276526216721945952246126427621672462764216742167421676156451992627687454871357351679654165168741163768741321313475341645664654462169877375734584149874883454687888846454967945465415648878785464
+console.log(biggestNumber)
+
+/*
+strings-> strore chars
+created by,
+double quote
+single quote
+back ticks<Template string>
+*/
+/*
+special-> \t >tab
+newline-> \n
+single quote escape-> \'
+double quote escape-> \"
+*/
+
+let str1="Double I am Alvin"
+console.log(str1)
+let str2='Single I am Alvin'
+console.log(str2)
+let str3=`Back Tick I am Alvin`
+console.log(str3)
+let str4="21" //Numbers in quotes count as strings
+console.log("21 is a string")
+let str5=`
+multi-line strings
+`
+
+/*Boolean rep true or false*/
+let alvinIsMale=true
+console.log(alvinIsMale)
+let alvinIsMarried=false
+console.log(alvinIsMarried)
+
+/*Null-> lack of value */
+let nullVal1=null
+console.log(nullVal1)
+
+/**undefined-> lack of value
+ * Never set explicitly
+ * use null instead 
+ * js engine sets for you
+ */
+let undefinedVal1=undefined// don't do this
+console.log(undefinedVal1)
+let undefinedVal2;
+console.log(undefinedVal2)
+
+/**Arrays
+ */
+let array1=[23,434,54,"gibberish"]
+console.log(array1)
