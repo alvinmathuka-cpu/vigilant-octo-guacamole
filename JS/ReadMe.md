80908883f2893328bdba7ca60fa9548e6d4175b9
alvinmathuka-cpu <alvin.mathuka@student.moringaschool.com>
2026-06-25 16:16:47 +0300

    Tax Calculator Part 2

---
+++ JS/ReadMe.md
@@ -0,0 +1,6 @@
+# Class Repo
+### Currently contains
+Javascript code and a small amount of html
+
+### How  commits are ordered 
+Infrequent commits, code was done after class
--- JS/taxCalculator/index.js
+++ JS/taxCalculator/index.js
@@ -8,6 +8,7 @@
  */
 //stay here until he has entered a correct number
 let grossMonthlySalary = null;
+const personalRelief = 2400;
 while (true) {
     let gross = prompt("Enter your Gross Salary");
     if (!isNaN(gross)) {
@@ -21,24 +22,60 @@ while (true) {
 }
 console.log(`Your gross is ${grossMonthlySalary} its type is ${typeof grossMonthlySalary}`);
 
+//NSSF
+let NSSF = null;
+let RawNSSF = null;
+let NSSFTier = null;
+
+if (grossMonthlySalary<= 9000) {
+    RawNSSF = grossMonthlySalary*0.06;
+    NSSFTier = `Tier 1`;
+}else if (grossMonthlySalary<108000) {
+    RawNSSF = 9000*0.06
+    +(grossMonthlySalary-9000)*0.06;
+    NSSFTier = `Tier 2`;
+} else {
+    RawNSSF = 6480;
+    NSSFTier = `Tier 3`;
+}
+NSSF = RawNSSF;
+console.log(`NSSF = ${NSSF} and Tier = ${NSSFTier}`)
+
+//Taxable Income
+
+let taxableIncome = grossMonthlySalary-NSSF;
+
 //Paye
+
 let paye = null;
 let tier = null;
 
-if (grossMonthlySalary >= 0 && grossMonthlySalary <= 24000){
-    paye = grossMonthlySalary*0.1;
+if (taxableIncome >= 0 && taxableIncome <= 24000){
+    paye = taxableIncome*0.1;
     tier = `0-24,000`;
-}else if(grossMonthlySalary >= 24001 && grossMonthlySalary <= 32333){
-    paye = grossMonthlySalary*0.25;
+}else if(taxableIncome <= 32333){
+    paye = 24000*0.1
+    +(taxableIncome-24000)*0.25;
     tier = `24,001-32,333`;
-}else if(grossMonthlySalary >= 32334 && grossMonthlySalary <= 500000){
-    paye = grossMonthlySalary*0.3;
+}else if(taxableIncome <= 500000){
+    paye = 24000*0.1
+    +8333*0.25
+    +(taxableIncome-24000-8333)*0.3;
     tier = `32,334-500,000`;
-}else if(grossMonthlySalary >= 500001 && grossMonthlySalary <= 800000){
-    paye = grossMonthlySalary*0.325;
+}else if(taxableIncome <= 800000){
+    paye = 24000*0.1
+    +8333*0.25
+    +467667*0.3
+    +(taxableIncome-467667-24000-8333)*0.325;
     tier = `500,001-800000`;
-}else if(grossMonthlySalary >= 800000){
-    paye = grossMonthlySalary*0.35;
+}else if(taxableIncome > 800000){
+    paye = 24000*0.10
+    +8333*0.25
+    +467667*0.30
+    +300000*0.325
+    +(taxableIncome - 800000)*0.35;
     tier = `>=800000`;
 }
-console.log(`paye = ${paye} tier = ${tier}`)
+console.log(`Taxable Income = ${taxableIncome}`)
+console.log(`paye = ${paye} Paye tier = ${tier}`)
+
