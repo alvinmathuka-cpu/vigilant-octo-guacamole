d81d7bf10722cf9724ba40e6102dcf00babee368
alvinmathuka-cpu <alvin.mathuka@student.moringaschool.com>
2026-06-29 15:17:49 +0300

    Junior-dev-commit-2 + Prettier-config

--- JS/junior-dev/introFunction.js
+++ JS/junior-dev/introFunction.js
@@ -4,8 +4,42 @@
  * has to be called to execute
  */
 
-function timeAlert(){
+function timeAlert() {
     const date = new Date();
-    alert(`time stamp: ${date.toISOString()}`)
+    alert(`time stamp: ${date.toISOString()}`);
 }
-timeAlert();
+//timeAlert();
+
+function areaOfTriangle(base, height) {
+    console.log(`base is ${base} its type ${typeof base}`);
+    console.log(`height is ${height} its type ${typeof height}`);
+    const area = 0.5 * base * height;
+    console.log(
+        `For traingle with base ${base} and height ${height} area is ${area}`
+    );
+}
+
+//scenario 1
+
+areaOfTriangle();
+
+//scenario 2
+
+areaOfTriangle(20);
+
+//scenario 3
+
+areaOfTriangle(20, 30);
+
+//scenario 4
+
+let base1 = 50;
+let height1 = 60;
+areaOfTriangle(base1, height1);
+
+//scenario 5
+
+let base2 = 'hello';
+let height2 = true;
+areaOfTriangle(base2, height2);
+//console.log(`${typeof areaOfTriangle}`)
---
+++ JS/junior-dev/return.js
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+
+function sayMyName() {
+    let firstName = 'Samson';
+    console.log(`First Name ${firstName}`);
+    let secondName = 'Johnna';
+    console.log(`Second Name ${secondName}`);
+    let fullName = `${firstName} ${secondName}`;
+    console.log(`Names are ${fullName}`);
+    return true;
+}
+
+let sayMyNameValue = sayMyName();
+
+console.log(
+    `Function returned ${sayMyNameValue} its type is ${sayMyNameValue}`
+);
